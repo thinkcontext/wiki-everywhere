86ea7ed431740c46a7d30a09318157ad61d5999f
thinkcontext <contact@thinkcontext.org>
2018-08-22 17:38:44 -0400

    remove youtube, add nyt and politifact

--- chrome/js/handlebars-templates.js
+++ chrome/js/handlebars-templates.js
@@ -27,6 +27,22 @@ templates['genericInfo'] = template({"1":function(container,depth0,helpers,parti
 
   return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.wikidata : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(4, data, 0),"data":data})) != null ? stack1 : "");
 },"useData":true});
+templates['rss'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "  <li>\r\n    <a href=\""
+    + alias4(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"link","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</a> \r\n  </li>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : {};
+
+  return "<h3>"
+    + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h3>\r\n\r\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 templates['rule'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
@@ -37,27 +53,5 @@ templates['rule'] = template({"compiler":[7,">= 4.0.0"],"main":function(containe
     + "</a></b> with a key of <b>"
     + alias4(((helper = (helper = helpers.key || (depth0 != null ? depth0.key : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"key","hash":{},"data":data}) : helper)))
     + "</b>.\r\n";
-},"useData":true});
-templates['youtube'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
-
-  return "  <li>\r\n    <a href=\"https://youtube.com/watch?v="
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.id : depth0)) != null ? stack1.videoId : stack1), depth0))
-    + "\">"
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.snippet : depth0)) != null ? stack1.title : stack1), depth0))
-    + "</a>\r\n    <a href=\"https://youtube.com/watch?v="
-    + alias2(alias1(((stack1 = (depth0 != null ? depth0.id : depth0)) != null ? stack1.videoId : stack1), depth0))
-    + "\">\r\n      <img src=\""
-    + alias2(alias1(((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0.snippet : depth0)) != null ? stack1.thumbnails : stack1)) != null ? stack1["default"] : stack1)) != null ? stack1.url : stack1), depth0))
-    + "\"\r\n        height=\""
-    + alias2(alias1(((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0.snippet : depth0)) != null ? stack1.thumbnails : stack1)) != null ? stack1["default"] : stack1)) != null ? stack1.height : stack1), depth0))
-    + "\"\r\n        width=\""
-    + alias2(alias1(((stack1 = ((stack1 = ((stack1 = (depth0 != null ? depth0.snippet : depth0)) != null ? stack1.thumbnails : stack1)) != null ? stack1["default"] : stack1)) != null ? stack1.width : stack1), depth0))
-    + "\">\r\n    </a>\r\n  </li>\r\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1;
-
-  return "<h3>Youtube videos</h3>\r\n\r\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.items : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 })();
--- readme.md
+++ readme.md
@@ -3,14 +3,14 @@
 Wiki Everywhere is a web browser extension which leverages Wikidata in order
 to overlay linked open data (LOD) onto the wider web.
 
-![Example using the Rotten Tomatoes entry for the Dark Knight](https://raw.githubusercontent.com/thinkcontext/wiki-everywhere/master/misc/screen-dk.png)
+![Example of retrieving Marco Rubio's New York Times and Politifact latest entries on his Twitter profile page](https://raw.githubusercontent.com/thinkcontext/wiki-everywhere/master/misc/rubio-nyt-poli.png)
 
 #### Using it
 
-Clone this repository, open Chrome in developer mode, and load the "chrome" folder as an unpacked extension.  If you want to use the Youtube search functionality, you'll need an API key from Google.  Once you have it, rename chrome/js/settings.js-example to settings.js (excluded by .gitignore) and put the key in the file.
+Clone this repository, open Chrome in developer mode, and load the "chrome" folder as an unpacked extension.  
 
 #### How it works
 
-Wiki Everywhere looks at the URL of the current page and attempts to match it against a list of patterns maintained using Wikidata's [formatter URL property](https://www.wikidata.org/wiki/Property:P1630) of authority control identifiers.  If the URL matches, a search of Wikidata is performed using the derived property and key value.  If the search returns a match for an entity then the extension is able to use the data there to perform additional queries of the LOD cloud.
+Wiki Everywhere looks at the URL of the current page and attempts to match it against a list of patterns maintained using Wikidata's [formatter URL property](https://www.wikidata.org/wiki/Property:P1630) of authority control identifiers.  If the URL matches, a search of Wikidata is performed using the derived property and key value.  If the search returns a match for an entity then the extension is able to use the data there to perform additional queries to display relevant linked information.
 
-In the example above, the user visits, the [Rotten Tomatoes entry for "The Dark Knight"](http://www.rottentomatoes.com/m/the_dark_knight).  The URL matches the authority control formatter property of "http://www.rottentomatoes.com/$1" for ["Rotten Tomatoes identifier" (P1258)](https://www.wikidata.org/wiki/Property:P1258).  Thus, a SPARQL search of Wikidata can be performed using the derived property of P1258 with a key of "m/the_dark_knight".  An entity ([Q163872](https://www.wikidata.org/wiki/Q163872)) is found and the query returns the Wikipedia page and the official website property (P856) if there is one.  The Freebase property (P646) is used to perform a subsequent search against Youtube's API to display related videos.
+In the example above, the user visits, [Marco Rubio's Twitter profile](https://twitter.com/marcorubio).  The URL matches the authority control formatter property of "https://twitter.com/$1" for ["Twitter username" (P2002)](https://www.wikidata.org/wiki/Property:P2002).  Thus, a SPARQL search of Wikidata can be performed using the derived property of P2002 with a key of "marcorubio".  An entity ([Q324546](https://www.wikidata.org/wiki/Q324546)) is found and the query returns the Wikipedia page and the NYTimes Topic and Politifact property values.  Those values are used to retrieve the RSS feeds from those sites, allowing their latest entries to be displayed.  Those 2 properties are meant to illustrate that any Wikidata entry could be used to access an API to display contextually relevant 3rd party information.
